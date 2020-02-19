e3996a787b05fcd3b49f9f32d1ebdbc44bcca9b7
gajalux <balasundaramgajaluxan@gmail.com>
2020-02-19 15:47:53 +0530

    My first funwork

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
-
+  //tamil songs*
 $(document).ready(function() {
   SC.stream('/tracks/729399541',function(sound){
     $('#start-t1').click(function(e) {
@@ -35,6 +35,39 @@ $(document).ready(function() {
            sound.stop();
         });
   });
+    SC.stream('/tracks/317969885',function(sound){
+      $('#start-t4').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+      $('#stop-t4').click(function(e) {
+             e.preventDefault();
+             sound.stop();
+          });
+    });
+
+    SC.stream('/tracks/67875890',function(sound){
+      $('#start-t5').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+      $('#stop-t5').click(function(e) {
+             e.preventDefault();
+             sound.stop();
+          });
+    });
+
+    SC.stream('/tracks/511333437',function(sound){
+      $('#start-t6').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+      $('#stop-t6').click(function(e) {
+             e.preventDefault();
+             sound.stop();
+          });
+    });
+    //telugu songs*
   SC.stream('/tracks/742634620',function(sound){
       $('#start-te1').click(function(e) {
              e.preventDefault();
@@ -45,7 +78,7 @@ $(document).ready(function() {
              sound.stop();
           });
     });
-    SC.stream('/playlists/649716513',function(sound){
+    SC.stream('/tracks/256712655',function(sound){
         $('#start-te2').click(function(e) {
                e.preventDefault();
                sound.start();
@@ -55,7 +88,7 @@ $(document).ready(function() {
                sound.stop();
             });
       });
-      SC.stream('/playlists/276063855',function(sound){
+      SC.stream('/tracks/256712208',function(sound){
           $('#start-te3').click(function(e) {
                  e.preventDefault();
                  sound.start();
@@ -65,6 +98,37 @@ $(document).ready(function() {
                  sound.stop();
               });
         });
+        SC.stream('/tracks/522633336',function(sound){
+            $('#start-te4').click(function(e) {
+                   e.preventDefault();
+                   sound.start();
+                 });
+            $('#stop-te4').click(function(e) {
+                   e.preventDefault();
+                   sound.stop();
+                });
+          });
+          SC.stream('/tracks/480614625',function(sound){
+              $('#start-te5').click(function(e) {
+                     e.preventDefault();
+                     sound.start();
+                   });
+              $('#stop-te5').click(function(e) {
+                     e.preventDefault();
+                     sound.stop();
+                  });
+            });
+            SC.stream('/tracks/256712492',function(sound){
+                $('#start-te6').click(function(e) {
+                       e.preventDefault();
+                       sound.start();
+                     });
+                $('#stop-te6').click(function(e) {
+                       e.preventDefault();
+                       sound.stop();
+                    });
+              });
+        //malayalam songs*
         SC.stream('/tracks/679510445',function(sound){
             $('#start-m1').click(function(e) {
                    e.preventDefault();
@@ -75,7 +139,7 @@ $(document).ready(function() {
                    sound.stop();
                 });
           });
-          SC.stream('/playlists/115451742',function(sound){
+          SC.stream('/tracks/208635620',function(sound){
               $('#start-m2').click(function(e) {
                      e.preventDefault();
                      sound.start();
@@ -95,5 +159,35 @@ $(document).ready(function() {
                        sound.stop();
                     });
               });
+              SC.stream('/tracks/337543156',function(sound){
+                  $('#start-m4').click(function(e) {
+                         e.preventDefault();
+                         sound.start();
+                       });
+                  $('#stop-m4').click(function(e) {
+                         e.preventDefault();
+                         sound.stop();
+                      });
+                });
+                SC.stream('/tracks/275752671',function(sound){
+                    $('#start-m5').click(function(e) {
+                           e.preventDefault();
+                           sound.start();
+                         });
+                    $('#stop-m5').click(function(e) {
+                           e.preventDefault();
+                           sound.stop();
+                        });
+                  });
+                  SC.stream('/tracks/296030957',function(sound){
+                      $('#start-m6').click(function(e) {
+                             e.preventDefault();
+                             sound.start();
+                           });
+                      $('#stop-m6').click(function(e) {
+                             e.preventDefault();
+                             sound.stop();
+                          });
+                    });
 
 });
